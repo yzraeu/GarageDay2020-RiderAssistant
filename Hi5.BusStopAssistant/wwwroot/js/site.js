8d74fe836858e40772b1078e3992003c554c7f36
Israel Rodriguez <yzraeu@gmail.com>
2020-03-13 00:20:15 -0700

    add howler as audio lib

--- Hi5.BusStopAssistant/wwwroot/js/site.js
+++ Hi5.BusStopAssistant/wwwroot/js/site.js
@@ -1,39 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿let language = window.navigator.userLanguage || window.navigator.language;
 
 $(function () {
+    let playingAudio = false;
 
-    //let audioPlaying = true, backgroundAudio, browser;
-    //browser = navigator.userAgent.toLowerCase();
-    //$('<audio class="audio1" src="/audios/Audio_en_CA.mp3" loop></audio>').prependTo('body');
-    //if (!browser.indexOf('firefox') > -1) {
-    //    $('<embed id="background-audio" src="/audios/Audio_en_CA.mp3" autostart="1"></embed>').prependTo('body');
-    //    backgroundAudio = setInterval(function () {
-    //        $("#background-audio").remove();
-    //        $('<embed id="background-audio" src="/audios/Audio_en_CA.mp3"></embed>').prependTo('body');
-    //    }, 120000); // 120000 is the duration of your audio which in this case 2 mins.
-    //}
+    let sound = new Howl({
+        src: [`/audios/Audio_${language}.mp3`, `/audios/Alert_${language}.mp3`]
+    });
 
-    //var audioPlayer = $(".audio-player")[0];
-    //var alertPlayer = $(".alert-player")[0];
+    sound.once('load', function () {
+        sound.play();
+    });
 
-    ////audioPlayer.play();
-    //audioPlayer.onended = function () {
-    //    alertPlayer.play();
-    //};
+    sound.on('end', function () {
+        console.log('Finished!');
+    });
+
 
     $("#mute-button").on('click', function (e) {
         let button = $(this);
-        if (button.hasClass("fa-volume-up")) {
-            //audioPlayer.stop();
-            //alertPlayer.stop();
+        if (playingAudio) {
+            sound.stop();
             button.removeClass("fa-volume-up").addClass("fa-volume-mute");
         } else {
-            //audioPlayer.play();
+            sound.play();
             button.addClass("fa-volume-up").removeClass("fa-volume-mute");
 
         }
+        playingAudio = !playingAudio;
     });
 });
